ca4d4f465ab8e1bdbc9d4da64f8875eaa03bfa25
krimkrimmm <147626046+krimkrimmm@users.noreply.github.com>
2025-05-06 16:24:54 +0700

    javaspring-buoi18

---
+++ src/main/java/vn/scrip/buoi18/Buoi18Application.java
@@ -0,0 +1,25 @@
+//Yêu cầu
+//Học viên hiển thị danh sách phim + phân trang:
+//
+//Danh sách phim lẻ
+//Danh sách phim bộ
+//Danh sách phim chiếu rạp
+package vn.scrip.buoi18;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class Buoi18Application
+{
+	public static void main(String[] args) {
+		SpringApplication.run(Buoi18Application.class, args);
+	}
+}
+
+
+
+
+
+
+
+
